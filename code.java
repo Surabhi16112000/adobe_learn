7901cc2de2bf2c04708de4fbc2eb8dd8a57752b8
Surabhi Singh <surabhi.sohar@gmail.com>
2021-05-28 17:05:10 +0530

    my first commit

---
+++ code.java
@@ -0,0 +1,16 @@
+class Solution{
+    static long disarrange(int N)
+    {
+        long mod=1000000007;
+        long[] dp=new long[N+1];
+        dp[0]=0;
+        dp[1]=0;
+        dp[2]=1;
+        for(int i=3;i<=N;i++)
+        {
+            dp[i]=((i-1)*(dp[i-1]+dp[i-2]))% mod;
+        }
+        return dp[N];
+        
+    }
+}
